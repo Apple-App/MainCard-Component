b9523e004adbffa40d97407d6ecb26f07488b448
Ari <ari.efron@gmail.com>
2019-01-17 09:33:53 -0500

    Adds morgan

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const compression = require("compression");
 const app = express();
 const path = require('path')
+const morgan = require('morgan');
 
 const pool = require('../db/config.js')
 
@@ -16,10 +17,12 @@ const routes = require("./routes/routes.js");
 app.use(cors());
 
 app.use(bodyParser.json());
+app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, "/../public")));
 app.use(compression());
 
-console.log(path.join(__dirname, "/../public"))
+console.log(process.env.pgun, process.env.pgpw, process.env.pghost)
+
 
 app.use("/movies", routes);
 
